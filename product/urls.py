84f61992341799547d3a16444b2e8b87ac00cd3d
Vadim <VadimPokhabov@mail.ru>
2024-09-11 18:04:13 +0700

    Add urls from product

---
+++ product/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from product.apps import ProductConfig
+from product.views import FactoryProductViewSet, NetworkProductViewSet, BusinessmanProductViewSet
+
+app_name = ProductConfig.name
+
+router = routers.DefaultRouter()
+router.register(r'factory_product', FactoryProductViewSet)
+router.register(r'network_product', NetworkProductViewSet)
+router.register(r'businessman_product', BusinessmanProductViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
