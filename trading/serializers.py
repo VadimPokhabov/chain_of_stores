a448ee8c248a5f47738d1da3af1fb1435ff67320
Vadim <VadimPokhabov@mail.ru>
2024-09-11 16:04:06 +0700

    Add serializers from trading

---
+++ trading/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Factory, Network, Businessman
+
+
+class FactorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Factory
+        fields = '__all__'
+        read_only_fields = ['arrears']
+
+
+class NetworkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Network
+        fields = '__all__'
+        read_only_fields = ['arrears']
+
+
+class BusinessmanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Businessman
+        fields = '__all__'
+        read_only_fields = ['arrears']
