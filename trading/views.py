da32f28417ef527f1c77155d4336403e0805819c
Vadim <VadimPokhabov@mail.ru>
2024-09-11 16:04:19 +0700

    Add view from trading

---
+++ trading/views.py
@@ -0,0 +1,33 @@
+from rest_framework import filters
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from trading.models import Factory, Network, Businessman
+from trading.serializers import FactorySerializer, NetworkSerializer, BusinessmanSerializer
+
+
+class FactoryViewSet(viewsets.ModelViewSet):
+    queryset = Factory.objects.all()
+    serializer_class = FactorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('country',)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = request.data.copy()
+        data.pop('arrears', None)  # Запрет обновления поля arrears
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+
+class NetworkViewSet(viewsets.ModelViewSet):
+    queryset = Network.objects.all()
+    serializer_class = NetworkSerializer
+
+
+class BusinessmanViewSet(viewsets.ModelViewSet):
+    queryset = Businessman.objects.all()
+    serializer_class = BusinessmanSerializer
