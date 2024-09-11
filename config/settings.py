c032cfd2bb6071213f2cfd82ec115993e3ac398a
Vadim <VadimPokhabov@mail.ru>
2024-09-11 10:26:56 +0700

    Add INSTALLED_APPS users, add AUTH_USER_MODEL

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -94,3 +95,6 @@ STATIC_URL = 'static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
+
