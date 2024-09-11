e2788478f14d1f1b704f04c396b4d677adf12a0c
Vadim <VadimPokhabov@mail.ru>
2024-09-11 18:04:06 +0700

    Add serializers from product

---
+++ product/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from product.models import FactoryProduct, NetworkProduct, BusinessmanProduct
+
+
+class FactoryProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FactoryProduct
+        fields = '__all__'
+
+
+class NetworkProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NetworkProduct
+        fields = '__all__'
+
+
+class BusinessmanProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusinessmanProduct
+        fields = '__all__'
