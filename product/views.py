ab4af81ea912378a6248997db5d86821d9624e58
Vadim <VadimPokhabov@mail.ru>
2024-09-11 18:04:19 +0700

    Add view from product

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from product.models import FactoryProduct, NetworkProduct, BusinessmanProduct
+from product.serializers import FactoryProductSerializer, NetworkProductSerializer, BusinessmanProductSerializer
+
+
+class FactoryProductViewSet(viewsets.ModelViewSet):
+    queryset = FactoryProduct.objects.all()
+    serializer_class = FactoryProductSerializer
+
+
+class NetworkProductViewSet(viewsets.ModelViewSet):
+    queryset = NetworkProduct.objects.all()
+    serializer_class = NetworkProductSerializer
+
+
+class BusinessmanProductViewSet(viewsets.ModelViewSet):
+    queryset = BusinessmanProduct.objects.all()
+    serializer_class = BusinessmanProductSerializer
