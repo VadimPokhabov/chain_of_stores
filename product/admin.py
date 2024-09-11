39bc0671de03d6a242b70b25a15ff4dad6bc3197
Vadim <VadimPokhabov@mail.ru>
2024-09-11 18:03:48 +0700

    Add admin.py from product

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import FactoryProduct, NetworkProduct, BusinessmanProduct
+
+
+@admin.register(FactoryProduct)
+class FactoryProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NetworkProduct)
+class NetworkProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BusinessmanProduct)
+class BusinessmanProductAdmin(admin.ModelAdmin):
+    pass
