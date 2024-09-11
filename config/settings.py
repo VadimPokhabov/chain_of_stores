9bd3e084c30d51c8973b87b37aefe6e340b68d4f
Vadim <VadimPokhabov@mail.ru>
2024-09-11 10:53:24 +0700

    Add INSTALLED_APPS, REST_FRAMEWORK settings

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'django_filters',
+
     'users',
+    'trading',
 ]
 
 MIDDLEWARE = [
@@ -97,4 +102,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        'users.permissions.IsActiveUser',
+    ),
+}
 
